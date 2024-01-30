021617e95f2edfabd01f51c9ddcd6cd38a43d292
oliprog-sys <140720020+oliprog-sys@users.noreply.github.com>
2024-01-30 19:39:41 +0300

    Update absoluteValue.cpp

--- absoluteValue.cpp
+++ absoluteValue.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 using namespace std;
 
-
-void increment(int& num) {
-    num++;
-}
-
 int main(){
-    /*int num;
+    int num;
     cout << "Enter a number: ";
     cin >> num;
 
@@ -16,16 +11,8 @@ int main(){
     }
 
     cout << "The absoulute value of the number is: " << num;
-    */
+    
 
-    /*int num[10] = {1};
-    cout << num[2];*/
-
-    int value = 5;
-    std::cout << "Before increment: " << value << std::endl;
-    increment(value);
-    std::cout << "After increment: " << value << std::endl;
-
-    cout << value << endl;
+    
     return 0;
 }
